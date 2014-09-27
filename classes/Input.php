315c92ec63a785e663c51ec67ec5f8710539fb10
Tharindu Ketipearachchi <katramesh91@gmail.com>
2014-09-27 19:50:03 +0530

    register validation finished

--- classes/Input.php
+++ classes/Input.php
@@ -1,8 +1,30 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+class Input {
+    public static function exists($type = 'post'){
+        switch ($type){
+        case 'post':
+            return (!empty($_POST)) ? TRUE : FALSE;
+            break;
+        case 'get':
+            return (!empty($_GET)) ? TRUE : FALSE;
+            break;
+        default :
+            return FALSE;
+            break;
+                
+        }
+        
+    }
+    public static function get($item){
+        if (isset($_POST[$item])){
+            return $_POST[$item];
+        }elseif (isset ($_GET[$item])) {
+            return $_GET[$item];
+            
+        }
+        return '';
+        
+    }
+}
