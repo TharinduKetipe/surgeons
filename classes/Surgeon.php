b159d033f0030c627eda327f0bcb9c0955ecfe51
Tharindu Ketipearachchi <katramesh91@gmail.com>
2014-09-28 12:01:06 +0530

    register fixed

--- classes/Surgeon.php
+++ classes/Surgeon.php
@@ -29,7 +29,7 @@ class Surgeon{
      
      public function create($fields=array()){
 		if (!$this->_db->insert('users', $fields)) {
-			//throw new Exception('There was a problem creating an account.');
+			throw new Exception('There was a problem creating an account.');
 			
 		}
 	}
